f51bec7576e3061fdb6fb69585d1697d7875a96f
SashaVons <vonswork@gmail.com>
2022-01-12 18:42:13 +0100

    fix: ios module

--- ios/Adpayment.m
+++ ios/Adpayment.m
@@ -1,8 +1,8 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(AdpaymentModule, NSObject)
+@interface RCT_EXTERN_MODULE(Adpayment, NSObject)
 
- RCT_EXTERN_METHOD(encryptCard:(NSString) cardNumber
+RCT_EXTERN_METHOD(encryptCard:(NSString) cardNumber
  expiryMonth:(NSString)expiryMonth
  expiryYear:(NSString) expiryYear
  securityCode:(NSString) securityCode
