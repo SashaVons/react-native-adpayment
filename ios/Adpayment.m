a30af20b043e08d579f67e132b0db9d28620cf7c
SashaVons <vonswork@gmail.com>
2022-01-13 17:02:12 +0100

    fix: ios redirect

--- ios/Adpayment.m
+++ ios/Adpayment.m
@@ -2,12 +2,15 @@
 
 @interface RCT_EXTERN_MODULE(Adpayment, NSObject)
 
-RCT_EXTERN_METHOD(encryptCard:(NSString) cardNumber
+RCT_EXTERN_METHOD(encrypt:(NSString) cardNumber
  expiryMonth:(NSString)expiryMonth
  expiryYear:(NSString) expiryYear
  securityCode:(NSString) securityCode
  publicKey:(NSString) publicKey
  resolver:(RCTPromiseResolveBlock)resolve
  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(openRedirect:(NSString) redirectData
+ clientKey:(NSString) clientKey
 
 @end
--- ios/Adpayment.swift
+++ ios/Adpayment.swift
@@ -3,21 +3,53 @@ import Foundation
 import SafariServices
 
 @objc(Adpayment)
-class Adpayment: NSObject {
+class Adpayment: NSObject, ActionComponentDelegate, PresentationDelegate {
+    func present(component: PresentableComponent) {
+        
+    }
+    
+    func didOpenExternalApplication(_ component: ActionComponent) {
+        
+    }
+    
+    func didProvide(_ data: ActionComponentData, from component: ActionComponent) {
+        
+    }
+    
+    func didComplete(from component: ActionComponent) {
+        
+    }
+    
+    func didFail(with error: Error, from component: ActionComponent) {
+        
+    }
+    
+    @objc func openRedirect(_ redirectData: String, clientKey: String) {
+        let actionComponent: AdyenActionComponent = {
+            let apiContext = APIContext(environment: Environment.test, clientKey: clientKey)
+            let component = AdyenActionComponent(apiContext: apiContext)
+            component.delegate = self
+            component.presentationDelegate = self
+            return component
+        }()
+        let data = Data(redirectData.utf8)
+        let action = try! JSONDecoder().decode(Action.self, from: data)
+        actionComponent.handle(action)
+    }
 
-    @objc func encryptCard(_ cardNumber: String,
+    @objc func encrypt(_ cardNumber: String,
                            expiryMonth:String,
                            expiryYear:String,
                            securityCode:String,
                            publicKey: String,
                            resolver resolve: RCTPromiseResolveBlock,
                            rejecter reject: RCTPromiseRejectBlock)  {
-        let card = CardEncryptor.Card(number: cardNumber,
+        let card = Card(number: cardNumber,
                                       securityCode: securityCode,
                                       expiryMonth:  expiryMonth,
                                       expiryYear: expiryYear)
      do {
-         let encryptedCard = try CardEncryptor.encryptedCard(for: card, publicKey: publicKey)
+         let encryptedCard = try CardEncryptor.encrypt(card: card, with: publicKey)
           
           let resultMap:Dictionary? = [
             "encryptedCardNumber":encryptedCard.number,
